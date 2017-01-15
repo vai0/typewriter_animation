e6e6bbe78940b213dd1c8682e7858b3e3672b83b
Justin Chi <j.chi2241@gmail.com>
2017-01-15 15:32:13 -0800

    clean up

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-// $(function() {
-  // $(document).foundation();
-  //
-  // //slideout.js menu bar instance
-  // var slideout = new Slideout({
-  //   'panel': document.getElementById('main-panel'),
-  //   'menu': document.getElementById('slideout-menu'),
-  //   'padding': 256,
-  //   'tolerance': 70
-  // });
-  //
-  // //hack for fixed header so the slideout.js menu works properly
-  // slideout.on('beforeopen', function() {
-  //   document.querySelector('.fixed').classList.add('fixed-open');
-  // });
-  //
-  // slideout.on('beforeclose', function() {
-  //   document.querySelector('.fixed').classList.remove('fixed-open');
-  // });
-  //
-  // document.querySelector('.toggle-button').addEventListener('click', function() {
-  //   slideout.toggle();
-  // });
-
-  //code for typewriter animation
-
-// });
-
 (function() {
     var str = "JUSTIN CHI";
     var text = '';
